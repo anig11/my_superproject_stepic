d746d4c3288daadefe8c56b0a61d27b8b773d511
anig11 <andrus.igor.ru>
2022-12-09 14:19:41 +0300

    Twelfth commit

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-def wsgi_application(environ, start_response):
+def application(environ, start_response):
     status = '200 OK'
     headers = [('Content-Type', 'text/plain')]
     body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
